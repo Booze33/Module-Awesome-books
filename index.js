3cf64cfbcbb347099bb9f923ee6978309ea007cd
Booze33 <tebetisloh@gmail.com>
2023-03-27 17:23:09 -0500

    Added modules

---
+++ index.js
@@ -0,0 +1,38 @@
+import Library from './modules/library.js';
+import displaySection from './modules/displaySection.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
+
+
+// let listBooks
+const listBooks = new Library();
+listBooks.className = 'purple';
+
+// ====================== NAVIGATION =========================
+function UpdateTime() {
+  const currentDate = `${DateTime.now().monthLong} ${DateTime.now().day} ${DateTime.now().year}, ${DateTime.now().hour}:${DateTime.now().minute}:${DateTime.now().second}`;
+  const dateElement = document.getElementById('date');
+  dateElement.innerText = currentDate;
+}
+
+UpdateTime ();
+setInterval(UpdateTime, 1000);
+
+// MENUS
+const listMenuLink = document.querySelector('#m-list');
+const addMenuLink = document.querySelector('#m-add');
+const contactMenuLink = document.querySelector('#m-contact');
+// Get all the section
+// const mainSection = document.querySelector('.main-section');
+
+
+listMenuLink.addEventListener('click', () => {
+  displaySection('book-list');
+});
+
+addMenuLink.addEventListener('click', () => {
+  displaySection('add-book');
+});
+
+contactMenuLink.addEventListener('click', () => {
+  displaySection('contact');
+});
---
+++ modules/library.js
@@ -0,0 +1,62 @@
+import Book from './book.js';
+
+class Library {
+    constructor() {
+      this.books = JSON.parse(localStorage.getItem('MY-Library')) || [];
+      this.section = document.querySelector('#book-list');
+      this.form = document.querySelector('#form');
+      this.title = document.querySelector('#title');
+      this.author = document.querySelector('#author');
+      this.addButton = document.getElementById('add-book-btn');
+  
+      this.addButton.addEventListener('click', () => this.addBook());
+      this.getBooks();
+    }
+  
+    addBook() {
+      const bookTitle = this.title.value;
+      const bookAuthor = this.author.value;
+      const objBook = new Book(bookTitle, bookAuthor);
+      this.books.push(objBook);
+      this.saveToLocalStorage();
+      this.getBooks();
+      this.clearInputs();
+    }
+  
+    removeBook(title, author) {
+      this.books = this.books.filter(
+        (objBook) => objBook.title !== title || objBook.author !== author,
+      );
+      this.saveToLocalStorage();
+      this.getBooks();
+    }
+  
+    // Local Storage
+    saveToLocalStorage() {
+      localStorage.setItem('MY-Library', JSON.stringify(this.books));
+    }
+  
+    getBooks() {
+      this.section.innerHTML = '';
+      this.books.forEach((book) => {
+        const div = document.createElement('div');
+        const removeButton = document.createElement('button');
+        removeButton.innerText = 'Remove';
+        removeButton.className = 'btn remove-btn';
+        removeButton.addEventListener('click', () => {
+          this.removeBook(book.title, book.author);
+        });
+        div.textContent = `${book.title} by ${book.author}`;
+        div.appendChild(removeButton);
+        div.className = 'tr';
+        this.section.appendChild(div);
+      });
+    }
+  
+    clearInputs() {
+      this.title.value = '';
+      this.author.value = '';
+    }
+  }
+
+  export default Library;
